b8b6be97a0db000c213b2745cb0ad6e0d734dbe6
G.Ishak <gishak@klevlb.com>
2026-01-02 14:37:01 +0200

    hide prlaod

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+'use client';
 
-'use client'; // 👈 Add this
+import { useState } from "react";
 import NewHero from "@/components/NewHero";
 import Events from "@/components/Events";
 import WelcomeSection from "@/components/WelcomeSection";
@@ -11,35 +12,37 @@ import ScrollPanels from "@/components/ScrollPanels";
 import ScrollImages from "@/components/ScrollImages";
 import Preloader from "@/components/Preloader";
 import RestaurantCarousel from "@/components/RestaurantCarousel";
-import { useState } from "react";
 import Column from "@/components/Column";
 import TypoAnimation from "@/components/TypoAnimation";
 import ParagraphLines from "@/components/ParagraphLines";
 import FoodCategoriesCarousel from "@/components/FoodCategoriesCarousel";
-import { Scroll } from "lucide-react";
 import ScrollImageGallery from "@/components/ScrollImageGallery";
+
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
     <>
+      {/* Preloader */}
       {isLoading && <Preloader onFinish={() => setIsLoading(false)} />}
-      {!isLoading && (
-        <>
-          <NewHero />
-                                                            <TypoAnim />
-                                                            <FoodCategoriesCarousel />
 
-
-   <ScrollImages />
-      <Column />
-   <TypoAnimation />
-   <RestaurantCarousel />
-   <ScrollImageGallery />
-   <RestaurantCarousel />
-        </>
-      )}
+      {/* Main content */}
+      <div
+        style={{
+          opacity: isLoading ? 0 : 1,
+          transition: 'opacity 0.5s ease-in-out'
+        }}
+      >
+        <NewHero />
+        <TypoAnim />
+        <FoodCategoriesCarousel />
+        <ScrollImages />
+        <Column />
+        <TypoAnimation />
+        <RestaurantCarousel />
+        <ScrollImageGallery />
+        <RestaurantCarousel />
+      </div>
     </>
   );
 }
-
